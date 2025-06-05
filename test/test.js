56efea6d465abb65845c7fd5dbdf99f2df1411b5
epibyte <boettcher.f@googlemail.com>
2025-06-05 20:38:47 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
-import Point from './src/Point.js';
-import Line from './src/Line.js';
+import Point from '../src/Point.js';
+import Line from '../src/Line.js';
 
 const p1 = new Point(10, 20);
 const p2 = new Point(30, 40);
-const line = new Line(p1, p2);
+const l1 = new Line(p1, p2);
 
-console.log(line.length());
+console.log(p1.toString());
+console.log(p2.toString());
+console.log(l1.toString()); // Line(Point(10, 20) -> Point(30, 40))
+console.log('Length:', l1.length());
+console.log('Midpoint:', l1.midPt().toString());
