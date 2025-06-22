20ea5d89d0c25717868eb4e006128da59f0d4be2
epibyte <boettcher.f@googlemail.com>
2025-06-22 17:15:02 +0200

    merge

--- src/Line.js
+++ src/Line.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Point from './Point.js';
 
 // Class Line: uses Point objects
@@ -31,38 +30,4 @@ export default class Line {
   toString() {
     return `Line(${this.pt1.toString()} -> ${this.pt2.toString()})`;
   }
-=======
-import Point from './Point.js';
-
-// Class Line: uses Point objects
-export default class Line {
-  constructor(pt1, pt2) {
-    if (!(pt1 instanceof Point) || !(pt2 instanceof Point)) {
-      throw new Error('Line constructor expects Point instances.');
-    }
-    this.pt1 = pt1;
-    this.pt2 = pt2;
-  }
-
-  length() {
-    return this.pt1.distanceTo(this.pt2);
-  }
-
-  midPt() {
-    return this.lerpPt(0.5);
-  }
-  lerpPt(f) {
-    return this.pt1.lerp(this.pt2, f);
-  }
-  lerpLine(otherLine, f) {
-    return new Line(
-      this.pt1.lerp(otherLine.pt1, f),
-      this.pt2.lerp(otherLine.pt2, f)
-    );
-  }
-
-  toString() {
-    return `Line(${this.pt1.toString()} -> ${this.pt2.toString()})`;
-  }
->>>>>>> 9e5460e9f73f30f518f6642937292b69848cbcbb
 }
--- src/Point.js
+++ src/Point.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  // Class Point: represents a 2D point
 export default class Point {
   constructor(x, y) {
@@ -37,44 +36,4 @@ export default class Point {
   toString() {
     return `Point(${this.x}, ${this.y})`;
   }
-=======
- // Class Point: represents a 2D point
-export default class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  static new(x = 0, y = 0) {
-    return new Point(x, y);
-  }
-  
-  distanceTo(otherPoint) {
-    const dx = this.x - otherPoint.x;
-    const dy = this.y - otherPoint.y;
-    return Math.sqrt(dx * dx + dy * dy);
-  }
-
-  lerp(otherPoint, f) {
-    return new Point(
-      this.x + (otherPoint.x - this.x) * f,
-      this.y + (otherPoint.y - this.y) * f
-    );
-  }
-
-  rotate(angle, origin = null) {
-    if (origin == null) origin = new Point();
-
-    let dx = pt.x - origin.x;
-		let dy = pt.y - origin.y;
-		let rotatedX = origin.x + dx * cos(angle) - dy * sin(angle);
-		let rotatedY = origin.y + dx * sin(angle) + dy * cos(angle);
-		
-    return new Point(rotatedX, rotatedY);
-  }
-
-  toString() {
-    return `Point(${this.x}, ${this.y})`;
-  }
->>>>>>> 9e5460e9f73f30f518f6642937292b69848cbcbb
 }
